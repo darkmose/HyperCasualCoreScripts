efb03dbbbc393a1c08123df18887b78c7753d61f
Maxim <pohomov1997@gmail.com>
2022-08-16 17:32:24 +0300

    -add core scripts in asset, -change canvas asset

--- Core/Enemy/EnemyData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyData : MonoBehaviour
-{
-    [SerializeField] private EnemyLifeSystem _enemyLifeSystem;
-    public EnemyLifeSystem LifeSystem => _enemyLifeSystem;
-}
